cb9b46422e80706ea82e11d24d9bceebaa912ba8
KonovalovNikolai <konovv.nikk@gmail.com>
2022-09-05 11:54:25 +0300

    Отображение текущей температуры

--- WeatherViewer/WeatherViewer/AssemblyInfo.cs
+++ WeatherViewer/WeatherViewer/AssemblyInfo.cs
@@ -1,3 +1,6 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("Nunito-Light.ttf", Alias = "Nunito-Light")]
+[assembly: ExportFont("Nunito-Regular.ttf", Alias = "Nunito")]
