fb679b8300c33e132a92cc3d259f4f83bdefe0d6
KonovalovNikolai <konovv.nikk@gmail.com>
2022-09-06 12:30:20 +0300

    Рефакторинг структуры проекта

--- WeatherViewer/WeatherViewer/Root/View/MainPage/MainPage.xaml.cs
+++ WeatherViewer/WeatherViewer/Root/View/MainPage/MainPage.xaml.cs
@@ -1,10 +1,5 @@
-﻿using OpenMeteoApi;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Xamarin.Essentials;
